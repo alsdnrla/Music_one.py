5b5f04a7ab7c12024cc0d5289202d6f6e67b217e
alsdnrla <86514801+alsdnrla@users.noreply.github.com>
2021-11-12 12:37:56 +0900

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -104,6 +104,7 @@ def load_chrome_driver():
     options.binary_location = os.getenv('GOOGLE_CHROME_BIN')
 
     options.add_argument('--headless')
+    options.add_argument("lang=ko_KR")
     # options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
 
