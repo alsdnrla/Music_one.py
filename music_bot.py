ae6b295fa46b3f55cd63df1b08a45c26aa5c8388
alsdnrla <86514801+alsdnrla@users.noreply.github.com>
2021-11-12 15:05:45 +0900

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -388,36 +388,7 @@ async def 목록재생(ctx):
             await ctx.send("노래가 이미 재생되고 있어요!")
 
 
-@bot.command()
-async def 멜론차트(ctx):
-    if not vc.is_playing():
-        
-        options = webdriver.ChromeOptions()
-        options.add_argument("headless")
 
-        global entireText
-        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            
-        driver = load_chrome_driver()
-        driver.get("https://www.youtube.com/results?search_query=멜론차트")
-        source = driver.page_source
-        bs = bs4.BeautifulSoup(source, 'lxml')
-        entire = bs.find_all('a', {'id': 'video-title'})
-        entireNum = entire[0]
-        entireText = entireNum.text.strip()
-        musicurl = entireNum.get('href')
-        url = 'https://www.youtube.com'+musicurl 
-
-        driver.quit()
-
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-        URL = info['formats'][0]['url']
-        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
-        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-    else:
-        await ctx.send("이미 노래가 재생 중이라 노래를 재생할 수 없어요!")
 
 @bot.command()
 async def 차트(ctx):
