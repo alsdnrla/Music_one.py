9a2f2ac52c801ed1f1581e88eee1aaeabb7c92ce
alsdnrla <86514801+alsdnrla@users.noreply.github.com>
2022-04-03 18:31:52 +0900

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -503,4 +503,4 @@ bot.add_cog(Music(bot))
 async def on_ready():
     print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
 
-bot.run("OTYwMDg0MDU4NDI2MjAwMTU0.YklR9Q.RhesNicL13zWS2TCKBn_9lpUa2k")
+bot.run("")
